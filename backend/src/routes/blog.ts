2fb3022c4f3b2fc0135e39505a0322702fe4c0e0
Mrinal Sharma <mrinalwork2@gmail.com>
2025-07-03 18:54:59 +0530

    changed routes config

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -101,7 +101,18 @@ blogRouter.get("/bulk", async (c) => {
       datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate());
   
-    const posts = await prisma.post.findMany();
+    const posts = await prisma.post.findMany({
+      select : {
+        content: true,
+        title : true,
+        id : true,
+        author : {
+          select : {
+            name : true
+          }
+        }
+      }
+    });
   
     return c.json({ posts });
   });
@@ -114,12 +125,20 @@ blogRouter.get("/:id", async (c) => {
   
   const id = c.req.param("id");
 
-  const body = await c.req.json();
   try {
     const post = await prisma.post.findFirst({
       where: {
         id: id,
       },
+      select : {
+        title: true,
+        content : true,
+        author : {
+          select : {
+            name : true
+          }
+        }
+      }
     });
 
     return c.json({ post });
